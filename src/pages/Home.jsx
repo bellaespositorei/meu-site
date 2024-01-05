c087c37b8ca0993d83cefecd427706b4091ed251
Isabella Esposito <142772766+bellaespositorei@users.noreply.github.com>
2024-01-04 22:18:59 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Presentation from "../components/Home";
+import Presentation from "../components/Presentation";
 
 const Home = () => {
   return (
